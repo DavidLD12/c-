f2e8d458ba386b864b833387fa43f5d80b368cfa
DavidLD12 <davidlopezsanvi@gmail.com>
2022-10-03 16:36:05 +0200

    Triangulo completado

--- triangulo.cs
+++ triangulo.cs
@@ -1,1 +1,39 @@
 /* Crear un triangulo */
+
+using System;
+
+class Triangulo
+{
+	static void Main()
+	{
+		int ancho;
+		
+		do
+		{
+			Console.WriteLine("Escribe el numero de la altura");
+			ancho = Convert.ToInt32(Console.ReadLine());
+			
+			if ( ancho % 2 == 0) {
+				Console.WriteLine("El numero no es impar");
+			}
+
+		}
+		while ( ancho % 2 == 0 );
+		
+		int espac;
+		int aster = ancho;
+		
+		for ( int i = ancho ; i >= 1 ; i-- ) {
+			espac = (ancho - aster)/2;
+			for ( int j = espac ; j >= 1 ; j-- ) {
+				Console.Write(" ");
+			}
+			for ( int j = aster ; j >= 1 ; j--) {
+				Console.Write("*");
+			}
+			aster-=2;
+			Console.WriteLine();
+		}
+	}
+}
+		
